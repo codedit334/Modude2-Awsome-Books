e8f00fef2fd1f3255bec0360dac8299847e2e929
ZewdieMc <zewdiehabtie26@gmail.com>
2023-02-13 14:24:56 +0300

    Fix Remove button bug

--- js/script.js
+++ js/script.js
@@ -6,57 +6,67 @@ const booksContainer = document.querySelector(".book-list");
 let booksArr = [];
 let bookObj = {};
 
-
-const getBooks = ()=>{
+const getBooks = () => {
   const books = localStorage.getItem("books");
-  if(books){
+  if (books) {
     booksArr = JSON.parse(books);
   }
-}
+};
 
-const renderBooks = ()=>{
+const renderBooks = () => {
   getBooks();
-const booksHTML = booksArr.map((book) => (`
+  const booksHTML = booksArr.map(
+    (book) => `
 <li data-id='${book.id}'>
   <div class="book-title">${book.title}</div>
   <div class="book-title">${book.desc}</div>
   <button class="remove_book">Remove</button>
   <hr>
 </li>
-`));
+`
+  );
+  booksContainer.innerHTML = booksHTML.join("");
 
-booksContainer.innerHTML = booksHTML.join("");
+};
+
+const addRemoveEvent = () => {
+  const removeButtons = document.querySelectorAll(".remove_book");
+  if (removeButtons)
+    removeButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log('clicked')
+        getBooks();
+        removeBook(event.target.parentNode.dataset.id);
+      });
+    });
 }
 
-document.querySelector(".form-add").addEventListener("click", (event) => {
-  event.preventDefault();
-
+const addBook = () => {
   if (bookTitleInput.value.length !== 0 && bookTitleInput.value.length !== 0) {
-    bookObj.id = Math.floor(Math.random() * 10000)
+    bookObj.id = Math.floor(Math.random() * 10000);
     bookObj.title = bookTitleInput.value;
     bookObj.desc = bookDescInput.value;
     booksArr.push(bookObj);
     localStorage.setItem("books", JSON.stringify(booksArr));
   }
-renderBooks()
-
-});
-
-window.onload = () => {
-  renderBooks()
+  renderBooks();
+  addRemoveEvent();
 }
 
+const removeBook = (bookId) => {
+  booksArr= booksArr.filter((book) => parseInt(book.id) !== parseInt(bookId));
+  localStorage.setItem("books", JSON.stringify(booksArr));
+  renderBooks();
+  addRemoveEvent()
+};
 
-const removeButtons = document.querySelectorAll(".book-list li .remove_book");
-
-
-console.log(removeButtons)
-
-removeButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
+window.onload = () => {
+  renderBooks();
+  addRemoveEvent();
+  document.querySelector(".form-add").addEventListener("click", (event) => {
     event.preventDefault();
-    console.log('hello')
-    booksArr = booksArr.filter((book) => book.id !== event.target.parentNode.getAttribute("data-id"));
-    localStorage.setItem("books", JSON.stringify(booksArr));
-    renderBooks();
-  })});
+    addBook();
+  });
+
+};
